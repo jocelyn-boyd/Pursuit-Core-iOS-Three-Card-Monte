8ea7957e2380b1cb8368087bf07c4c880e5782ca
Jocelyn Boyd <jocelynboyd@pursuit.org>
2019-08-13 19:46:01 -0400

    Completes setup for reset button function

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -16,20 +16,40 @@ class ViewController: UIViewController {
   }
 
   @IBOutlet weak var userMessage: UILabel!
+  @IBOutlet weak var rightCard: UIButton!
+  @IBOutlet weak var middleCard: UIButton!
+  @IBOutlet weak var leftCard: UIButton!
   
+  
+  //craeated three buttons, tagged them and connected to the function 'gamePlay'
   @IBAction func gamePlay(_ sender: UIButton) {
     
+//Make the winning button be randomly selected. Each game should have a randomly generated winning card.
     let winningNumber = Int.random(in: 1...3)
     if sender.tag == winningNumber {
+      rightCard.isEnabled = false
+      middleCard.isEnabled = false
+      leftCard.isEnabled = false
       print("You win!")
       print(winningNumber)
       print(sender.tag)
     } else {
+      rightCard.isEnabled = false
+      middleCard.isEnabled = false
+      leftCard.isEnabled = false
       print("Try Again!")
       print(winningNumber)
       print(sender.tag)
     }
   }
+  
+ //After a user clicks on a button, disable all buttons until the new game button is pressed. Reset the text to an introductory message.
+  @IBAction func resetButton(_ sender: UIButton) {
+    rightCard.isEnabled = true
+    middleCard.isEnabled = true
+    leftCard.isEnabled = true
+  }
+  
   
 
 }
