c1f4a65bd2c0574ce19155db10eb98cb3c522bd5
Jocelyn Boyd <jocelynboyd@pursuit.org>
2019-12-29 00:27:56 -0500

    refactor gamePlay function using a switch statement

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -10,59 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var userMessage: UILabel!
+   @IBOutlet weak var rightCard: UIButton!
+   @IBOutlet weak var middleCard: UIButton!
+   @IBOutlet weak var leftCard: UIButton!
+  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-  @IBOutlet weak var userMessage: UILabel!
-  @IBOutlet weak var rightCard: UIButton!
-  @IBOutlet weak var middleCard: UIButton!
-  @IBOutlet weak var leftCard: UIButton!
   
-  
-  //craeated three buttons, tagged them and connected to the function 'gamePlay'
+  //created three buttons, tagged them and connected to the function 'gamePlay'
   @IBAction func gamePlay(_ sender: UIButton) {
     
-//Make the winning button be randomly selected. Each game should have a randomly generated winning card.
-    let winningNumber = Int.random(in: 1...3)
-    if sender.tag == winningNumber {
+    //Randomly generate the winning number 
+    let number = Int.random(in: 1...3)
+    let winningCard = number
+    
+    switch sender.tag {
+    case winningCard:
       sender.setImage(UIImage(named: "kingCard"), for: .normal)
+      
       rightCard.isEnabled = false
       middleCard.isEnabled = false
       leftCard.isEnabled = false
-      userMessage.text = "Congrats! You win!"
-//      print(winningNumber)
-//      print(sender.tag)
-    } else {
-      let arrayOfButtons = [rightCard,middleCard,leftCard]
-      for loserCard in arrayOfButtons {
-        if loserCard!.tag == winningNumber {
-          loserCard?.setImage(UIImage(named: "kingCard"), for: .normal)
-          print("here")
-        }
-      }
+      
+      userMessage.text = "Congrats! You've won!"
+      print(number)
+      
+    default:
       sender.setImage(UIImage(named: "threeCard"), for: .normal)
-      print(winningNumber)
-      print(sender.tag)
-      
-      
-      
-//      if winningNumber == leftCard.tag {
-//        leftCard.setImage(UIImage(named: "kingCard"), for: .normal)
-//        print("True")
-//      } else if winningNumber == middleCard.tag {
-//        middleCard.setImage(UIImage(named: "kingCard"), for: .normal)
-//        print("True")
-//      } else if winningNumber == rightCard.tag {
-//        rightCard.setImage(UIImage(named: "kingCard"), for: .normal)
-//        print("True")
-//      }
       
       rightCard.isEnabled = false
       middleCard.isEnabled = false
       leftCard.isEnabled = false
-      userMessage.text = "Better luck next time! Try again!"
+      
+      userMessage.text = "Better luck next time! Try again."
+      print(number)
     }
   }
   
@@ -71,7 +55,7 @@ class ViewController: UIViewController {
     rightCard.isEnabled = true
     middleCard.isEnabled = true
     leftCard.isEnabled = true
-    userMessage.text = "Choose another card!"
+    userMessage.text = "Choose Your Card"
     
     rightCard.setImage(UIImage(named: "cardBackRed"), for: .normal)
     middleCard.setImage(UIImage(named: "cardBackRed"), for: .normal)
